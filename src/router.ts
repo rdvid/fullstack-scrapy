388f0469c280ed05bc104dc7fe5e220a3ec32327
Rafael <rafael_dvid@hotmail.com>
2024-02-26 15:09:20 -0300

    add middleware for check cache existence and search validations

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { handleSearch } from "./controller/search.controller";
+import { getCache } from "./middleware/get-cache";
+import { searchValidation } from "./middleware/search-validation";
 
 const route = Router();
 
-route.get('/api/scrape', handleSearch)
+route.get('/api/scrape', searchValidation, getCache,  handleSearch)
 
 export default route;
