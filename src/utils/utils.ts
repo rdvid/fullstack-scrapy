0a55c3d1c1df893112a057384e93c50b1508a164
Rafael <rafael_dvid@hotmail.com>
2024-02-26 15:11:19 -0300

    generic function aside from controller to DRY

---
+++ src/utils/utils.ts
@@ -0,0 +1,39 @@
+import cheerio from "cheerio";
+import { Product } from "../@types/product.type";
+import { axiosAmazonInstance as axios } from "../config/axios"
+import { AxiosError, AxiosRequestConfig } from "axios";
+
+export const getProducts = async (options: AxiosRequestConfig) => {
+    try {     
+        let { data } = await axios.get('', options);
+        let $ = cheerio.load(data);
+        const products: Product[] = [];
+    
+        $('div.puis-card-container')
+            .each((i, element)=>{
+                // catch product items
+                const title = $(element).find('h2').last().text().trim();
+                const amount = $(element).find('span.a-price>span.a-offscreen').first().text().replace('$', '');
+                const rating = $(element).find('i.a-icon.a-icon-star-small>span.a-icon-alt').first().text();
+                const reviewsCount = $(element).find('span.rush-component span.a-size-base.s-underline-text').first().text().replace(',', '.');
+                const imageUrl = $(element).find('div.s-image-square-aspect>img').prop('src');
+                let price = amount ? Number.parseFloat(amount) : 0;
+                
+                let product: Product = {
+                    title,
+                    price,
+                    rating,
+                    reviewsCount: Number(reviewsCount),
+                    imageUrl: imageUrl
+                }
+                products.push(product)
+            });
+    
+        return products;
+    } catch (error: any) {
+        console.error(error.message)
+        throw new Error(error.message)
+    }
+
+}
+
