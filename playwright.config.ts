15c53c47ee44adccb4d0b733669adb6e4f8c7a0a
Rafael <rafael_dvid@hotmail.com>
2024-02-27 13:37:25 -0300

    setting up playwright run base configs for reports and traces

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,22 +4,19 @@ export default defineConfig({
   testDir: './frontend/e2e',
   retries: 1,
   workers: 1,
+  outputDir: "frontend/e2e/e2e-results",
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html', { open: 'never' }]],
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'http://127.0.0.1:1234',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'off',
+    screenshot: 'only-on-failure'
   },
 
   /* Configure projects for major browsers */
   projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
