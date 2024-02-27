d7d4af7ac55ac0b4025056916ca6730f027ccd2b
Rafael <rafael_dvid@hotmail.com>
2024-02-27 13:38:21 -0300

    updating keyword not provided 404 validation

--- src/middleware/search-validation.ts
+++ src/middleware/search-validation.ts
@@ -2,15 +2,18 @@ import { Request, Response, NextFunction } from "express";
 
 const searchValidation = async (req: Request, res: Response, next: NextFunction) => {
     let {keyword} = req.query;
-
+    
     if(!keyword){
         return res.status(400).json({
             message: "keyword must be provided"
         })
     }
-
-    if(typeof keyword !== 'string'){
+    
+    keyword = keyword.toString().trim();
+    
+    if(Number(keyword)){
         return res.status(400).json({
+            status: 400,
             message: "keyword must be an string"
         })
     }
