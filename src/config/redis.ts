a7690fddad207dd62e27d2a8340d59ce158b3717
Rafael <rafael_dvid@hotmail.com>
2024-02-27 22:35:38 -0300

    including env for prod redis instance

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -2,10 +2,18 @@ import Redis from 'ioredis';
 import dotenv from 'dotenv';
 dotenv.config();
 
-// uncomment the host line if you just want to use locally through Docker
+//  DEV docker redis
+// const redis = new Redis({
+//     host: 'redis', 
+//     port: Number(process.env.REDIS_PORT) || 6379,
+// });
+
+//  PROD
 const redis = new Redis({
-    // host: 'redis', 
-    port: 6379,
+    host: process.env.REDIS_HOST, 
+    port: Number(process.env.REDIS_PORT) || 6379,
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD
 });
 
 redis.set('key', 100, 'EX', 300)
