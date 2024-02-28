a7e526d6591e4c2d19275098942554213bc9221b
Rafael <rafael_dvid@hotmail.com>
2024-02-27 22:06:11 -0300

    upgrading cheerio hook tags to more consistence

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -15,16 +15,16 @@ export const getProducts = async (options: AxiosRequestConfig) => {
                 const title = $(element).find('h2').last().text().trim();
                 const amount = $(element).find('span.a-price>span.a-offscreen').first().text().replace('$', '');
                 const rating = $(element).find('i.a-icon.a-icon-star-small>span.a-icon-alt').first().text();
-                const reviewsCount = $(element).find('span.rush-component span.a-size-base.s-underline-text').first().text().replace(',', '.');
-                const imageUrl = $(element).find('div.s-image-square-aspect>img').prop('src');
-                let price = amount ? Number.parseFloat(amount) : 0;
+                const reviewsCount = $(element).find('span.a-size-base.s-underline-text').first().text().replace(',', '.');
+                const imageUrl = $(element).find('img.s-image').prop('src');
+                let price = amount ? amount : "0";
                 
                 let product: Product = {
                     title,
                     price,
                     rating,
                     reviewsCount: Number(reviewsCount),
-                    imageUrl: imageUrl
+                    imageUrl
                 }
                 products.push(product)
             });
