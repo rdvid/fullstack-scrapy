f30e76e99a34d7906e4854f6aa3bc278a2f9b8ba
Rafael <rafael_dvid@hotmail.com>
2024-02-28 15:06:43 -0300

    including /api-docs route to swagger ui

--- src/router.ts
+++ src/router.ts
@@ -3,8 +3,12 @@ import { handleSearch } from "./controller/search.controller";
 import { getCache } from "./middleware/get-cache";
 import { searchValidation } from "./middleware/search-validation";
 
+import swaggerUi from 'swagger-ui-express';
+import { swagger } from './docs/swagger';
+
 const route = Router();
 
+route.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swagger));
 route.get('/api/scrape', searchValidation, getCache,  handleSearch)
 
 export default route;
