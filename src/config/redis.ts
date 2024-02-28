b14657c03acbdb0c425e22c3cb55038b7790c173
Rafael <rafael_dvid@hotmail.com>
2024-02-27 22:38:56 -0300

    setting develop branch

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -3,18 +3,18 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 //  DEV docker redis
-// const redis = new Redis({
-//     host: 'redis', 
-//     port: Number(process.env.REDIS_PORT) || 6379,
-// });
+const redis = new Redis({
+    // host: 'redis', 
+    port: Number(process.env.REDIS_PORT) || 6379,
+});
 
 //  PROD
-const redis = new Redis({
-    host: process.env.REDIS_HOST, 
-    port: Number(process.env.REDIS_PORT) || 6379,
-    username: process.env.REDIS_USERNAME,
-    password: process.env.REDIS_PASSWORD
-});
+// const redis = new Redis({
+//     host: process.env.REDIS_HOST, 
+//     port: Number(process.env.REDIS_PORT) || 6379,
+//     username: process.env.REDIS_USERNAME,
+//     password: process.env.REDIS_PASSWORD
+// });
 
 redis.set('key', 100, 'EX', 300)
 
